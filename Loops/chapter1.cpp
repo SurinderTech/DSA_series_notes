c2f319fc3db08820c588b59dd309fc98a2ab9a57
surinderTech <surinderkumar3182@gmail.com>
2026-03-20 06:01:25 +0530

    conditional statement

---
+++ Loops/chapter1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    for (int i = 100; i >= 1; i--)
+    {
+
+        cout << i << " ";
+
+        if (i % 10 == 0)
+
+        {
+            cout << endl;
+        }
+    }
+    return 0;
+}
