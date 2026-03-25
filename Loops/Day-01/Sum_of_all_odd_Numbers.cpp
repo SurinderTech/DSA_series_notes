8273ce19be68f070775b8f30f58dd2ab09c48227
surinderTech <surinderkumar3182@gmail.com>
2026-03-25 20:19:12 +0530

    update

--- Loops/Day-01/Sum_of_all_odd_Numbers.cpp
+++ Loops/Day-01/Sum_of_all_odd_Numbers.cpp
@@ -1,3 +1,24 @@
+// sum of odd numbers with while loop
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    int sum = 0;
+
+    while (i <= 100)
+    {
+        cout << i << " ";
+        sum += i;
+        i += 2;
+    }
+    cout << "\nsum is " << sum;
+    return 0;
+}
+
+/* {// with for loop sum of even numbers}
+
 #include <iostream>
 using namespace std;
 
@@ -12,3 +33,4 @@ int main()
     cout << "\nsum " << sum;
     return 0;
 }
+*/
