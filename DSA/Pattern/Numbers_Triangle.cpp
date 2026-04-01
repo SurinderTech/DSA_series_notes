c0640e3009169a97d29dad7cb65dd702c842fb0a
SurinderTech <surinderkumar3182@gmail.com>
2026-04-02 02:33:51 +0530

    update

---
+++ DSA/Pattern/Numbers_Triangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    for(int i = 0; i <= n; i++){ //outer loop
+         
+        for(int j = 0; j <= i; j++){ //inner loop 
+            cout << i;
+        }
+        cout << endl;
+    }
+    return 0;
+}
