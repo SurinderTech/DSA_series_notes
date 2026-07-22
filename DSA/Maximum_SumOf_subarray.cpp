69905a91c0e4666851df3bf56532e19c324566a8
SurinderTech <surinderkumar3182@gmail.com>
2026-07-22 11:47:19 +0530

    Add new DSA pattern problems

---
+++ DSA/Maximum_SumOf_subarray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    int arr[5] = { 1, 2, 3, 4, 5};
+    int maxSum = 0;
+
+    for(int st = 0; st < n; st++){
+        int currSum = 0;
+        for(int end = st; end < n; end++){
+              currSum += arr[end];
+
+        }
+        cout << " sum of subarray: " << maxSum << endl;
+    }
+    return 0;
+} 
